734d4c924cef7717e340317771e6544f7d6af87b
Malte Schink <mail@malteschink.de>
2019-07-11 23:11:19 +0200

    New Class Structure for Handler

--- src/main/java/Handler.java
+++ src/main/java/Handler.java
@@ -31,77 +31,81 @@ public class Handler {
     return 0;
   }
   
-  public static class CreateUser implements HTTPServer.ContextHandler {
-    @Override
-    public int serve(HTTPServer.Request request, HTTPServer.Response response) throws IOException {
-      Log.status("'CreateUser' Request");
-      Map<String, String> params = request.getParams();
-      pw = new PWAuth();
-      db = new DBConnection();
+  static class User {
+    public static class Create implements HTTPServer.ContextHandler {
+      @Override
+      public int serve(HTTPServer.Request request, HTTPServer.Response response) throws IOException {
+        Log.status("'CreateUser' Request");
+        Map<String, String> params = request.getParams();
+        pw = new PWAuth();
+        db = new DBConnection();
       
-      JSONObject header = new JSONObject();
-      JSONObject results = new JSONObject();
-      try {
-        db.update("INSERT INTO user SET vName=?, nName=?, emailAdr=?, hashPassword=?, role=?, plz=?",
-                params.get("vName"),
-                params.get("nName"),
-                params.get("emailAdr"),
-                pw.hash(params.get("password").toCharArray()),
-                params.get("role"),
-                params.get("plz")
-        );
-      } catch (NullPointerException e) {
-        Log.exception(e);
-        sendReponse(response, 400, header, results);
-      } catch (SQLException e) {
-        Log.exception(e);
-        sendReponse(response, 500, header, results);
-      }
-      return sendReponse(response, 200, header, results);
-    }
-  }
-  
-  public static class DeleteUser implements HTTPServer.ContextHandler {
-    @Override
-    public int serve(HTTPServer.Request request, HTTPServer.Response response) throws IOException {
-      Log.error("test");
-      JSONObject header = new JSONObject();
-      JSONObject results = new JSONObject();
-      return sendReponse(response, 400, header, results);
-      
-    }
-  }
-  
-  public static class LoginUser implements HTTPServer.ContextHandler {
-    @Override
-    public int serve(HTTPServer.Request request, HTTPServer.Response response) throws IOException {
-      Map<String, String> params = request.getParams();
-      
-      JSONObject header = new JSONObject();
-      JSONObject results = new JSONObject();
-      String storedPassword, userID;
-      
-      try {
-        ResultSet resultSet = db.execute("SELECT Id,hashPassword FROM user WHERE emailAdr=?",
-                params.get("emailadr"));
-        resultSet.next();
-        
-        userID = resultSet.getString("id");
-        storedPassword = resultSet.getString("hashPassword");
-      } catch (SQLException e) {
-        Log.error("Collection of Password failed");
-        Log.exception(e);
-        return sendReponse(response, 500, header, results);
-      }
-      
-      if (pw.authenticate(params.get("password").toCharArray(), storedPassword)) {
-        //If login is succesfull user is being redirected back to Referer with http-status 303
+        JSONObject header = new JSONObject();
+        JSONObject results = new JSONObject();
+        try {
+          db.update("INSERT INTO user SET vName=?, nName=?, emailAdr=?, hashPassword=?, role=?, plz=?",
+                  params.get("vName"),
+                  params.get("nName"),
+                  params.get("emailAdr"),
+                  pw.hash(params.get("password").toCharArray()),
+                  params.get("role"),
+                  params.get("plz")
+          );
+        } catch (NullPointerException e) {
+          Log.exception(e);
+          sendReponse(response, 400, header, results);
+        } catch (SQLException e) {
+          Log.exception(e);
+          sendReponse(response, 500, header, results);
+        }
         return sendReponse(response, 200, header, results);
-      } else {
+      }
+    }
+  
+    public static class Delete implements HTTPServer.ContextHandler {
+      @Override
+      public int serve(HTTPServer.Request request, HTTPServer.Response response) throws IOException {
+        Log.error("test");
+        JSONObject header = new JSONObject();
+        JSONObject results = new JSONObject();
         return sendReponse(response, 400, header, results);
+      
+      }
+    }
+  
+    public static class Login implements HTTPServer.ContextHandler {
+      @Override
+      public int serve(HTTPServer.Request request, HTTPServer.Response response) throws IOException {
+        Map<String, String> params = request.getParams();
+      
+        JSONObject header = new JSONObject();
+        JSONObject results = new JSONObject();
+        String storedPassword, userID;
+      
+        try {
+          ResultSet resultSet = db.execute("SELECT Id,hashPassword FROM user WHERE emailAdr=?",
+                  params.get("emailadr"));
+          resultSet.next();
+        
+          userID = resultSet.getString("id");
+          storedPassword = resultSet.getString("hashPassword");
+        } catch (SQLException e) {
+          Log.error("Collection of Password failed");
+          Log.exception(e);
+          return sendReponse(response, 500, header, results);
+        }
+      
+        if (pw.authenticate(params.get("password").toCharArray(), storedPassword)) {
+          //If login is succesfull user is being redirected back to Referer with http-status 303
+          return sendReponse(response, 200, header, results);
+        } else {
+          return sendReponse(response, 400, header, results);
+        }
       }
     }
   }
+  
+  
   
   public static class Test implements HTTPServer.ContextHandler {
     @Override
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,9 +13,14 @@ public class Main {
     
     host.addContext("/test", new Handler.Test());
     
-    host.addContext("/user/create", new Handler.CreateUser(), "POST");
-    host.addContext("/user/login", new Handler.LoginUser(), "POST");
-    host.addContext("/user/delete", new Handler.DeleteUser());
+    host.addContext("/user/create", new Handler.User.Create(), "POST");
+    host.addContext("/user/login" , new Handler.User.Login(), "POST");
+    host.addContext("/user/delete", new Handler.User.Delete());
+  
+    host.addContext("/event/create", new Handler.Test());
+    host.addContext("/event/delete", new Handler.Test());
+    host.addContext("/event/update", new Handler.Test());
+  
     server.start();
   
     Log.success("Server started");
